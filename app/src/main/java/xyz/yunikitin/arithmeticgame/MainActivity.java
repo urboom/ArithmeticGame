9c523a06da2c66d255711d9a1617324ff71a4b80
Yuriy Nikitin <yurec116@gmail.com>
2015-12-30 14:39:05 +0200

    Add Score and Play Activity

--- app/src/main/java/xyz/yunikitin/arithmeticgame/MainActivity.java
+++ app/src/main/java/xyz/yunikitin/arithmeticgame/MainActivity.java
@@ -1,13 +1,97 @@
 package xyz.yunikitin.arithmeticgame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button playButton, recordButton, exitButton;
+    private String[] level = {"Easy", "Medium", "Hard"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        playButton = (Button) findViewById(R.id.play_button);
+        playButton.setOnClickListener(this);
+        recordButton = (Button) findViewById(R.id.high_button);
+        recordButton.setOnClickListener(this);
+        exitButton = (Button) findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.play_button:
+                //playActivity run
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Choose a level").setSingleChoiceItems(level,
+                        0, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                startPlay(which);
+                            }
+                        });
+                AlertDialog ad = builder.create();
+                ad.show();
+                break;
+            case R.id.high_button:
+                //recordsActivity run
+                Intent highIntent = new Intent(this, RecordsActivity.class);
+                this.startActivity(highIntent);
+                break;
+            case R.id.exit_button:
+                //exit program
+               openQuitDialog();
+                break;
+        }
+    }
+
+    private void startPlay(int chosenLevel) {
+        Intent playIntent = new Intent(this, PlayActivity.class);
+        playIntent.putExtra("level", chosenLevel);
+        this.startActivity(playIntent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        openQuitDialog();
+    }
+
+    private void openQuitDialog() {
+        AlertDialog.Builder quitDialog = new AlertDialog.Builder(
+                MainActivity.this);
+        quitDialog.setTitle("Exit: Are you sure?");
+
+        quitDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        quitDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        quitDialog.show();
     }
 }
--- app/src/main/java/xyz/yunikitin/arithmeticgame/RecordsActivity.java
+++ app/src/main/java/xyz/yunikitin/arithmeticgame/RecordsActivity.java
@@ -1,13 +1,36 @@
 package xyz.yunikitin.arithmeticgame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class RecordsActivity extends AppCompatActivity {
+
+public class RecordsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_records);
+
+        //get text view
+        TextView scoreView = (TextView)findViewById(R.id.high_scores_list);
+        //get shared prefs
+        SharedPreferences scorePrefs = getSharedPreferences(PlayActivity.GAME_PREFS, 0);
+        //get scores
+        String[] savedScores = scorePrefs.getString("highScores", "").split("\\|");
+        //build string
+        StringBuilder scoreBuild = new StringBuilder("");
+        for(String score : savedScores){
+            scoreBuild.append(score+"\n");
+        }
+        //display scores
+        scoreView.setText(scoreBuild.toString());
     }
+
 }
