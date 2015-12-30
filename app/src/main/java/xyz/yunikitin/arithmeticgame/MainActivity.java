c184976db515737b9760db84b8b9613c9628de65
Yuriy Nikitin <yurec116@gmail.com>
2015-12-30 16:46:40 +0200

    Add animation and localization

--- app/src/main/java/xyz/yunikitin/arithmeticgame/MainActivity.java
+++ app/src/main/java/xyz/yunikitin/arithmeticgame/MainActivity.java
@@ -8,12 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button playButton, recordButton, exitButton;
-    private String[] level = {"Easy", "Medium", "Hard"};
+
+    private String[] level;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +29,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         setContentView(R.layout.activity_main);
 
+        level = getResources().getStringArray(R.array.levels);
+
+        Animation buttonAnim = AnimationUtils.loadAnimation(this, R.anim.button_anim);
+        Animation textAnim = AnimationUtils.loadAnimation(this, R.anim.text_anim);
+
         playButton = (Button) findViewById(R.id.play_button);
         playButton.setOnClickListener(this);
+        playButton.startAnimation(buttonAnim);
         recordButton = (Button) findViewById(R.id.high_button);
         recordButton.setOnClickListener(this);
+        recordButton.startAnimation(buttonAnim);
         exitButton = (Button) findViewById(R.id.exit_button);
         exitButton.setOnClickListener(this);
+        exitButton.startAnimation(buttonAnim);
+
+        TextView infoTextView = (TextView) findViewById(R.id.intro);
+        infoTextView.startAnimation(textAnim);
+
 
     }
 
@@ -40,7 +56,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.play_button:
                 //playActivity run
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Choose a level").setSingleChoiceItems(level,
+                builder.setTitle(R.string.level_name).setSingleChoiceItems(level,
                         0, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
@@ -76,16 +92,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private void openQuitDialog() {
         AlertDialog.Builder quitDialog = new AlertDialog.Builder(
                 MainActivity.this);
-        quitDialog.setTitle("Exit: Are you sure?");
+        quitDialog.setTitle(R.string.dialog_title);
 
-        quitDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        quitDialog.setPositiveButton(R.string.dialog_yes, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 finish();
             }
         });
 
-        quitDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+        quitDialog.setNegativeButton(R.string.dialog_no, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
